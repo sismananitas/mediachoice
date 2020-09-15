c2a2589c397aae8dc0a89daefc4c93292aa83d8f
fernando <fer68769@gmail.com>
2020-09-15 13:14:39 -0500

    Se agrego logo con proximamente a Welcom y se agrego browser Sync

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,21 @@
-@extends('layouts.main')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-@section('content')
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <title>MEDIA CHOICE</title>
+</head>
+<body>
     <div class="container">
-        <h1>Media Choice</h1>
+        <div class="logo d-flex">
+            <img src="/images/logo_mediachoice.png" alt="Logo Media Choice">
+            <h1>PROXIMAMENTE</h1>
+        </div>
     </div>
-@endsection
+</body>
+</html>
